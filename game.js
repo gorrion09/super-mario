0d7c5cf60a99134394bcdec04a0f6f0539a384f1
gorrion09 <rbalaguerpico@gmail.com>
2025-03-17 23:16:21 +0100

    Añadido assets

--- game.js
+++ game.js
@@ -1,1 +1,30 @@
-console.log("Hello World");
+/* global Phaser */
+
+const { Phaser } = require("./phaser.min")
+
+const config = {
+    type: Phaser.AUTO,
+    width: 256,
+    height: 244,
+    backgroundColor: '#fff',
+    parent: 'game',
+    scene: {
+        preload,
+        create,
+        upload
+    }
+}
+
+new Phaser.Game(config)
+
+function preload() {
+    console.log('preload')
+}
+
+function create() {
+    console.log('create')
+}
+
+function upload() {
+    console.log('update')
+}
